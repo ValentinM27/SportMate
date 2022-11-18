23140688580621d95148ba1596e54f4e0d2592dc
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-11-18 09:12:48 +0100

    [front] - Change api link

--- SportMate_front/sportmate/lib/logic/apis/link_api.dart
+++ SportMate_front/sportmate/lib/logic/apis/link_api.dart
@@ -1,3 +1,3 @@
 class LinkApi{
-  static const String link = "http://appart.dialyx.fr:9670/MyS1CLj3JOBZqShqkdUyAz4tZkggWigKvDTtwUnT/api/";
+  static const String link = "http://127.0.0.1:9670/MyS1CLj3JOBZqShqkdUyAz4tZkggWigKvDTtwUnT/api/";
 }
